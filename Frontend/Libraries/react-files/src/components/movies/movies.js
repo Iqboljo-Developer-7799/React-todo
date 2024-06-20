aca75f0fe03111f03c8ce75eeab05ace950df684
Iqboljon-Developer-7799 <uiqboljon7799@gmail.com>
2024-06-20 21:33:00 +0500

    changes tracked

--- Frontend/Libraries/react-files/src/components/movies/movies.js
+++ Frontend/Libraries/react-files/src/components/movies/movies.js
@@ -5,10 +5,9 @@ import Movie from "../movie/movie";
 const Movies = ({ data }) => {
   return (
     <div className="movies">
-      {data.map((data, index) => {
-        <Movie key={index} data={data} />;
-      })}
-      {/* <Movie data={data} />; */}
+      <Movie data={data} />
+      <Movie data={data} />
+      <Movie data={data} />
     </div>
   );
 };
